48ee5066d494a9737e426f0a20d6b682c9caae21
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-11 21:19:09 +0000

    automatically backed up by learn

---
+++ lib/patient.rb
@@ -0,0 +1,21 @@
+class Patient 
+  attr_accessor 
+  
+  @@all=[]
+  
+  def initialize(name)
+    @name = name 
+    @@all<<self 
+  end 
+  
+  def new_appointment(doctor, date)
+    
+  
+  
+  def self.all 
+    @@all 
+  end 
+  
+  
+  
+end 
