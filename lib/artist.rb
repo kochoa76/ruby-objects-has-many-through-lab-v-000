784b397e75f656275dd7fcdf334aa56f798d1dab
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-11 19:58:05 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,7 +15,7 @@ class Artist
   
   def new_song(name, genre)
   song = Song.new(name, genre)
-    song.artist= self if song.artist != self 
+    song.artist== self if song.artist != self 
   end 
   
   
