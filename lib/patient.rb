38d97cd3af3ff07d191f68a3d276b21a501adda5
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-11 21:58:03 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,28 +1,21 @@
-class Patient 
-  attr_accessor :name 
-  @@all = []
-  
-  def initialize(name)
-    @name = name 
-    @@all<<self
-  end 
-  
-  def new_appointment(date, doctor)
-    Appointment.new(date, self, doctor)
-  end 
-  
-  def appointments 
-    Appointment.all.select do |appointment|
-    appointment.patient == self 
-  end 
-end 
+class Patient
+  attr_accessor :name
 
-  def doctors 
-    appointments.collect do |appointments|
-    appointments.doctor 
-  end 
-end 
-  
-  end 
-  
+  def initialize(name)
+    @name = name
+  end
+
+  def new_appointment(doctor, date)
+    Appointment.new(self, doctor, date)
+  end
+
+  def appointments
+    Appointment.all.select {|appointment| appointment.patient == self}
+  end
+
+  def doctors
+    appointments.map{|appointment| appointment.doctor}
+  end
+end
+
   
