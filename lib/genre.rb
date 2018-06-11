1a0b87336ee3fa4c542754e11cc6ca579e16f73e
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-11 20:23:47 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,11 +1,17 @@
 class Genre 
   
   attr_accessor :name
+  @@all = []
   
   def initialize(name)
     @name =name 
-    @songs =[]
+    @@all<<self 
   end 
+  
+  def self.all 
+    @@all 
+  end 
+  
   
   
   
