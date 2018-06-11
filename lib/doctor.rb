0edc4a3cf6abe395f11fa32db89e9b31d90ecd95
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-11 21:40:09 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -15,4 +15,10 @@ class Doctor
     Appointment.new(date, patient, self)
   end 
   
+  def appointments
+  Appointment.all.select do |appointment|
+  appointment.doctor == self 
+end 
+end 
+  
 end 
